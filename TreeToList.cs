1dc38b0e08e99dcb3fa58a5e91406e9c9cb563fc
Robert Bleattler <rbleattler@outlook.com>
2021-07-21 08:43:57 -0400

    junk/testing

---
+++ TreeToList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
+
+namespace StringOps {
+    public class TreeToList {
+        List<String> list = new List<String>();
+    }
+}
