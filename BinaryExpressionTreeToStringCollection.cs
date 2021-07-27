96d7e8598210eac4b8e664a2d293c8e2e33ce182
Robert Bleattler <Robert.Bleattler@ubc.com>
2021-07-27 13:08:56 -0400

    added (basic) error handling for string expressions with unbalanced parentheses.

--- BinaryExpressionTreeToStringCollection.cs
+++ BinaryExpressionTreeToStringCollection.cs
@@ -1,33 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using NReco.Linq;
 
-namespace StringOps
-{
+namespace StringOps {
     public class BinaryExpressionTreeParser : BinaryExpressionTreeUtilities {
-            public BinaryExpressionTreeParser () { }
-            public LambdaParser LambdaParser = new LambdaParser ();
+        public BinaryExpressionTreeParser () { }
+        public LambdaParser LambdaParser = new LambdaParser ();
 
-            public ArrayList ToStringCollection (string treeAsString) {
-                return ToStringCollection (LambdaParser.Parse (treeAsString) as BinaryExpression);
-            }
-            public ArrayList ToStringCollection (BinaryExpression treeExpression) {
-                ArrayList outerList = new ArrayList ();
-                ArrayList referenceList = new ArrayList ();
-                BuildList (treeExpression, ref referenceList, ref outerList);
-                return outerList;
-            }
-
-            public ArrayList BuildList (BinaryExpression evalExpression, ref ArrayList referenceList, ref ArrayList mainList) {
-                return BuildList (evalExpression, ref referenceList, ref mainList, false);
-            }
-            public dynamic Test (string StringToTest) {
-                try {
-                    return ToStringCollection (StringToTest);
-                } catch (System.Exception ex) {
-                    return ex;
+        public string LevelString (string inputString) {
+            var chars = inputString.ToCharArray ();
+            int leftOccurrences = Array.FindAll<char> (chars, x => x == '(').Count (); 
+            int rightOccurrences = Array.FindAll<char> (chars, x => x == ')').Count ();
+            if (leftOccurrences != rightOccurrences) {
+                if (leftOccurrences > rightOccurrences) {
+                    Debug.WriteLine("More Left");
+                    inputString = inputString + ")";
+                } else {
+                    Debug.WriteLine("More Right");
+                    inputString = "(" + inputString;
                 }
             }
-
+            return inputString;
         }
+
+        public ArrayList ToStringCollection (string treeAsString) {
+            treeAsString = LevelString(treeAsString);
+            BinaryExpression parsedExpression = LambdaParser.Parse (treeAsString) as BinaryExpression;
+            return ToStringCollection (parsedExpression);
+        }
+        public ArrayList ToStringCollection (BinaryExpression treeExpression) {
+            ArrayList outerList = new ArrayList ();
+            ArrayList referenceList = new ArrayList ();
+            BuildList (treeExpression, ref referenceList, ref outerList);
+            return outerList;
+        }
+
+        public ArrayList BuildList (BinaryExpression evalExpression, ref ArrayList referenceList, ref ArrayList mainList) {
+            return BuildList (evalExpression, ref referenceList, ref mainList, false);
+        }
+        public dynamic Test (string StringToTest) {
+            try {
+                return ToStringCollection (StringToTest);
+            } catch (System.Exception ex) {
+                return ex;
+            }
+        }
+
     }
+}
--- scripts/TestFunctionality.ps1
+++ scripts/TestFunctionality.ps1
@@ -1,8 +1,9 @@
 Set-Location "$PSScriptRoot\.."
+$Here = $PSScriptRoot
 Add-Type -Path @(
     #Idk why it doesn't load for me... if this causes issues, just comment it out
     "~\.nuget\packages\nreco.lambdaparser\*\lib\netstandard2.0\NReco.LambdaParser.dll", 
-    "$PSScriptRoot\..\bin\Debug\netcoreapp3.1\StringOps.dll"
+    "$Here\..\bin\Debug\netcoreapp3.1\StringOps.dll"
 )
 $TreeParser = [StringOps.BinaryExpressionTreeParser]::new();
 $LambdaParser = [NReco.Linq.LambdaParser]::new()
@@ -13,6 +14,7 @@ $Strings = @{
     String3 = "(param_1 && param_2 && (param_3 || param_4) && (param_5 || param_6 || param_7))"
     String4 = "(param1 || param8 || ((param2 || param3) && param4 && (param5 || param6 || param7)))"
     String5 = "(param1 || param8 && (param2 && param3 && param4 && (param5 || param6 || param7)))"
+    String6 = "(interface_id || (interface_name && (device_id || device_name))" # Bad String
 }
 $i = 0
 $($Strings.Keys | Sort-Object).ForEach{
@@ -28,7 +30,8 @@ $($Strings.Keys | Sort-Object).ForEach{
     $Out.ForEach{
         $Host.UI.WriteLine([System.ConsoleColor]::Cyan, $Host.UI.RawUI.BackgroundColor, $($_ -join ', '))
     }
+    $VarName = "Out$i"
+    Set-Variable -Name $VarName -Value $Out
     $Host.UI.WriteLine([System.ConsoleColor]::Green, $Host.UI.RawUI.BackgroundColor, "`n")
     $i++
 }
-
